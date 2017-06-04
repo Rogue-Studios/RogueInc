522dfe416d9d7845804cac5b972ebe8d8884b17f
Oliver Stenning <ostenning@gmail.com>
2017-06-04 23:32:54 +0100

    Created constructors for Factory and Resource classes.

--- src/Factory.java
+++ src/Factory.java
@@ -3,20 +3,110 @@ public class Factory {
     private String name;
     private boolean running;
     private boolean unlocked;
-    private Resource resource1;
-    private Resource resource2;
-    private Resource resource3;
-    private Resource resource4;
-    private int resource1Quantity;
-    private int resource2Quantity;
-    private int resource3Quantity;
-    private int resource4Quantity;
+    private int resourceTypes;
+    private Resource[] resources;
+    private int[] resourcesQuantity;
     private double marketValue;
     private double productionTime;
     private double timeSinceProduction;
 
-    public Factory() {
-        
+    public Factory(String name, Resource resource1, Resource resource2, Resource resource3, Resource resource4, int resource1Quantity, int resource2Quantity, int resource3Quantity, int resource4Quantity, double marketValue, double productionTime) {
+
+        this.name = name;
+        this.running = false;
+        this.unlocked = false;
+
+        this.resources[0] = resource1;
+        this.resources[1] = resource2;
+        this.resources[2] = resource3;
+        this.resources[3] = resource4;
+        this.resourceTypes = 0;
+
+        for(int x = 0; x < 4; x++) {
+            if(this.resources[x] != null) {
+                this.resourceTypes += 1;
+            }
+        }
+
+        this.resourcesQuantity[0] = resource1Quantity;
+        this.resourcesQuantity[1] = resource2Quantity;
+        this.resourcesQuantity[2] = resource3Quantity;
+        this.resourcesQuantity[3] = resource4Quantity;
+
+        this.marketValue = marketValue;
+        this.productionTime = productionTime;
+        this.timeSinceProduction = 0;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public void setUnlocked(boolean unlocked) {
+        this.unlocked = unlocked;
+    }
+
+    public void setResourceTypes(int resourceTypes) {
+        this.resourceTypes = resourceTypes;
+    }
+
+    public void setResources(Resource[] resources) {
+        this.resources = resources;
+    }
+
+    public void setResourcesQuantity(int[] resourcesQuantity) {
+        this.resourcesQuantity = resourcesQuantity;
+    }
+
+    public void setMarketValue(double marketValue) {
+        this.marketValue = marketValue;
+    }
+
+    public void setProductionTime(double productionTime) {
+        this.productionTime = productionTime;
+    }
+
+    public void setTimeSinceProduction(double timeSinceProduction) {
+        this.timeSinceProduction = timeSinceProduction;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public boolean isUnlocked() {
+        return unlocked;
+    }
+
+    public int getResourceTypes() {
+        return resourceTypes;
+    }
+
+    public Resource[] getResources() {
+        return resources;
+    }
+
+    public int[] getResourcesQuantity() {
+        return resourcesQuantity;
+    }
+
+    public double getMarketValue() {
+        return marketValue;
+    }
+
+    public double getProductionTime() {
+        return productionTime;
+    }
+
+    public double getTimeSinceProduction() {
+        return timeSinceProduction;
+    }
 }
--- src/Resource.java
+++ src/Resource.java
@@ -13,8 +13,116 @@ public class Resource {
     private int storerCount;
     private double speedModifier;
 
-    public Resource() {
+    public Resource(String name, double producerCost, double storerCost, double marketValue, double productionTime) {
+
+        this.name = name;
+        this.unlocked = false;
+        this.producerCost = producerCost;
+        this.storerCost = storerCost;
+        this.marketValue = marketValue;
+        this.currentStorage = 0;
+        this.maxStorage = 0;
+        this.productionTime = productionTime;
+        this.timeSinceProduction = 0;
+        this.producerCount = 0;
+        this.storerCount = 0;
+        this.speedModifier = 1;
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public boolean isUnlocked() {
+        return unlocked;
+    }
+
+    public double getProducerCost() {
+        return producerCost;
+    }
+
+    public double getStorerCost() {
+        return storerCost;
+    }
+
+    public double getMarketValue() {
+        return marketValue;
+    }
+
+    public int getCurrentStorage() {
+        return currentStorage;
+    }
+
+    public int getMaxStorage() {
+        return maxStorage;
+    }
+
+    public double getProductionTime() {
+        return productionTime;
+    }
+
+    public double getTimeSinceProduction() {
+        return timeSinceProduction;
+    }
+
+    public int getProducerCount() {
+        return producerCount;
+    }
+
+    public int getStorerCount() {
+        return storerCount;
+    }
+
+    public double getSpeedModifier() {
+        return speedModifier;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUnlocked(boolean unlocked) {
+        this.unlocked = unlocked;
+    }
+
+    public void setProducerCost(double producerCost) {
+        this.producerCost = producerCost;
+    }
+
+    public void setStorerCost(double storerCost) {
+        this.storerCost = storerCost;
+    }
+
+    public void setMarketValue(double marketValue) {
+        this.marketValue = marketValue;
+    }
+
+    public void setCurrentStorage(int currentStorage) {
+        this.currentStorage = currentStorage;
+    }
+
+    public void setMaxStorage(int maxStorage) {
+        this.maxStorage = maxStorage;
+    }
+
+    public void setProductionTime(double productionTime) {
+        this.productionTime = productionTime;
+    }
+
+    public void setTimeSinceProduction(double timeSinceProduction) {
+        this.timeSinceProduction = timeSinceProduction;
+    }
+
+    public void setProducerCount(int producerCount) {
+        this.producerCount = producerCount;
+    }
+
+    public void setStorerCount(int storerCount) {
+        this.storerCount = storerCount;
+    }
+
+    public void setSpeedModifier(double speedModifier) {
+        this.speedModifier = speedModifier;
+    }
 }
