48e4c54cfa81c342688906b585ff61aa76589cd1
Daniel <daniel.meechan.1@gmail.com>
2017-06-04 23:28:32 +0100

    Adding core files

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("resourcesView.fxml"));
+        primaryStage.setTitle("Rogue Inc.");
+        primaryStage.setScene(new Scene(root, 480, 854));
         primaryStage.show();
     }
     //comment
