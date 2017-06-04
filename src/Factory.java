c243141d9a5b9eac650ee78caaa447a58c2a8a0d
Oliver Stenning <ostenning@gmail.com>
2017-06-04 23:10:39 +0100

    Added Factory and Resource classes and variables.

---
+++ src/Factory.java
@@ -0,0 +1,22 @@
+public class Factory {
+
+    private String name;
+    private boolean running;
+    private boolean unlocked;
+    private Resource resource1;
+    private Resource resource2;
+    private Resource resource3;
+    private Resource resource4;
+    private int resource1Quantity;
+    private int resource2Quantity;
+    private int resource3Quantity;
+    private int resource4Quantity;
+    private double marketValue;
+    private double productionTime;
+    private double timeSinceProduction;
+
+    public Factory() {
+        
+    }
+
+}
---
+++ src/Resource.java
@@ -0,0 +1,20 @@
+public class Resource {
+
+    private String name;
+    private boolean unlocked;
+    private double producerCost;
+    private double storerCost;
+    private double marketValue;
+    private int currentStorage;
+    private int maxStorage;
+    private double productionTime;
+    private double timeSinceProduction;
+    private int producerCount;
+    private int storerCount;
+    private double speedModifier;
+
+    public Resource() {
+
+    }
+
+}
