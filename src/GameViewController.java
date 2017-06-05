8c3f942596eb86e1c77bfbed101deef8c96bdcce
Daniel <daniel.meechan.1@gmail.com>
2017-06-05 17:43:41 +0100

    Added FXML items to GameViewController

--- src/GameViewController.java
+++ src/GameViewController.java
@@ -1,3 +1,28 @@
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class GameViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameViewController implements Initializable{
+    // use @FXML injection to avoid overwriting the FXML View's objects and causing problems
+    @FXML private JFXListView<Resource> resourcesListView;
+    @FXML private JFXListView<Factory> productionListView;
+    //@FXML private JFXListView<Research> researchListView;
+    @FXML private JFXButton leaderboardButton;
+    @FXML private JFXButton moneyButton;
+    @FXML private JFXButton settingsButton;
+
+    private ObservableList<Resource> resourcesList;
+    private ObservableList<Factory> factoryList;
+    //private private ObservableList<Research> researchList;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
